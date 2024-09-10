100d9b00e7bd161541743c788d26f28630c9aa89
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-09-10 21:28:41 +0530

    Add files via upload

---
+++ OOPS/Abstraction/abstraction.cpp
@@ -0,0 +1,51 @@
+/*
+    - Displaying Only Essential information and hiding the details.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Customer{
+    string name;
+    int account_number,balance;
+    public:
+
+    Customer(string name, int account_number, int balance){
+        this->name = name;
+        this->account_number = account_number;
+        this->balance = balance;
+    }
+
+    void deposit(int amount) {
+        if(amount>0)
+        {
+            balance += amount;
+        }
+    }
+
+    void withdraw(int amount) {
+        if(amount<=balance&&amount>0) {
+            balance -= amount;
+        }
+    }
+
+    void display() {
+        cout<<name<<" "<<account_number<<" "<<balance<<endl;
+    }
+
+};
+
+int main() {
+
+    Customer A1("Rohit", 1, 1000);
+    Customer A2("Mohit", 2, 1800);
+    Customer A3("Mohan", 3, 2000);
+    A1.deposit(200);
+    A1.withdraw(500);
+    A1.display();
+
+
+
+    return 0;
+}
+
