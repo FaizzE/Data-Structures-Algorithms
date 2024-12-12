ea2a5a455f33bd483da1cb2be0bd9d0eddf11e00
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-12-12 16:58:26 +0530

    Update tableUsingRecursion.cpp

--- Recursion/tableUsingRecursion.cpp
+++ Recursion/tableUsingRecursion.cpp
@@ -5,11 +5,9 @@ void printTable(int N, int M) {
     if (M == 0) {
         return;
     }
-
     printTable(N, M - 1);  // Correct order: decrement M, keeping N fixed
     cout << N << " X " << M << " = " << M * N << endl;
 }
-
 int main() {
     int n = 25;  // Number for which we want the multiplication table
     int m = 10;  // Number of terms in the multiplication table
